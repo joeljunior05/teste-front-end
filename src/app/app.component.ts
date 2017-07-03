8050d1e811248963d020097ba9e31eb631d127d6
Joel Junior <joeljunior05@hotmail.com>
2017-07-03 11:14:26 -0300

    Readme.md changes and App template generation, ng new teste-front-end --minimal

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './page/app.component.html',
+  styleUrls: ['./style/app.component.css']
+})
+export class AppComponent {
+  title: string;
+}
